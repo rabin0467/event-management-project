c2b1a3110ec6ed9f19fccb4a2382b4c1932a91bb
Rabin <m.rabinmia467@gmail.com>
2025-07-22 18:08:52 +0600

    Final

--- event_management/urls.py
+++ event_management/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include("events.urls")),
-    
+    path("", lambda request:redirect("events/event-list/"))
 ]+debug_toolbar_urls()
